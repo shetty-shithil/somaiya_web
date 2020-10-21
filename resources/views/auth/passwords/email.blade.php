1560a1815d966e8ea02f932d5a0c22b1f34472e6
shetty-shithil <shithilshetty05@gmail.com>
2020-10-21 23:35:10 +0530

    Reset Password after logging in.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -18,7 +18,21 @@
 
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
+                        @if(Auth::user())
+                            {{-- <input type="hidden" id="email" name="event_id" value="{{Auth::user()->email}}"> --}}
+                            {{-- <input id="email" type="email hidden" class="form-control @error('email') is-invalid @enderror" name="email" value="{{Auth::user()->email}}" required autocomplete="email" autofocus> --}}
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{Auth::user()->email}}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        @else
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -32,7 +46,7 @@
                                 @enderror
                             </div>
                         </div>
-
+                        @endif
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -40,6 +54,10 @@
                                 </button>
                             </div>
                         </div>
+                        {{-- @if(Auth::user())
+                        Hello
+                    @endif --}}
+          
                     </form>
                 </div>
             </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,12 +37,66 @@
 
                     {{ __('You are logged in!!!!!!') }}
                 </div>
+                <div class="form-group row mb-0">
+
+                        @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Reset Password') }}
+                            </a>
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 <div id="fullcard_with_comments" class="modal">
+    <div class="modal-content">
+        <div class="row ml-28 m-0">
+            <div class="col m8 s12">
+                <h6 class="bold" id="day_on_modal"></h6>
+                <h6 class="bold" id="date_on_modal"></h6>
+                <h6 class="bold" id="time_on_modal"></h6>
+                <h6 class="bold mt-0" id="title_on_modal"></h6>
+                <p id="description_on_modal"></p>
+                <div class="row m-0 p-0">
+                    <div class="col m6 pr-20 s12">
+                        <span class="bold">Venue : </span><span id="venue_on_modal"></span>
+                    </div>
+                    <div>
+                        <span class="bold">Speaker : </span><span id="speaker_on_modal"></span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col m6 pr-20 s12">
+                        <span class="bold">Organiser : </span><span id="organiser_on_modal"></span>
+                    </div>
+                    <div>
+                        <span class="bold">Stakeholder : </span><span id="stakeholder_on_modal"></span>
+                    </div>
+                </div>
+                <button class="btn col m4 s5  mar-15 btn-modify modal-close">Modify</button>
+            </div>
+            <div class="col m4 s12">
+                <h5 class="bold top-0">Comments</h5>
+                <div class="view_comments">
+                    <div class="comment_1">
+                        <span class="faculty">HOD: </span> Please change the date
+                    </div>
+                    <div class="comment_2">
+                        <span class="faculty">DOA: </span> Please change the time and also change the venue
+                    </div>
+                    <div class="comment_3">
+                        <span class="faculty">PRINCIPAL: </span> Please change the name.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- <div id="fullcard_with_comments" class="modal">
     <div class="modal-content">
         <div class="row ml-28 m-0">
             <div class="col m8 s12">
@@ -84,9 +138,54 @@
             </div>
         </div>
     </div>
-</div>
-
+</div> --}}
 <div id="MyEvents">
+    <div class="container" style="padding: 50px 0;">
+        <!-- Events -->
+        <div class="card-head">
+            <div class="row card_to_update_modal" id="1"><a class="modal-trigger" href="#fullcard_with_comments">
+                <div class="col s12 m-0 p-0 offset-m2">
+                    <div class="card-panel black-text event m-0">
+                        <div class="valign-wrapper">
+                            <div class="row m-0">
+                                <div class="col m2 s12 p-0 card-date" style="transform: translateY(62%);">
+                                    <h6 class="bold" id="day_on_card1">Saturday</h6>
+                                    <h6 class="bold" id="date_on_card1">September 04</h6>
+                                </div>
+                                <div>
+                                    <h5 class="col m2 p-0 m-0" style="line-height:160px;" id="time_on_card1"> 11:00 - 12:00</h5>
+                                </div>
+                                <div>
+                                    <div class="col m8 p-0 m-0">
+                                        <h6 class="bold mt-0" id="title_on_card1">Seminar Details</h6>
+                                        <p id="description_on_card1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam consequuntur sapiente ab vero, expedita facere aspernatur repellat doloremque amet, dolorem quia, similique vel quam maxime alias nam. Ea, cumque officia.</p>
+                                        <div class="row m-0" style="display: none;">
+                                            <div class="col m6 s12 pr-20">
+                                                <span class="bold-sm">Venue : </span><span id="venue_on_card1">IT Building</span>
+                                            </div>
+                                            <div class="col m6 pad-20">
+                                                <span class="bold-sm">Speaker : </span> <span id="speaker_on_card1">XYZ</span>
+                                            </div>
+                                            <div class="col m6 s12 pr-20">
+                                                <span class="bold-sm">Organiser : </span> <span id="organiser_on_card1">XYZ Department</span>
+                                            </div>
+                                            <div class="col m6 s12 pad-20">
+                                                <span class="bold-sm">Stakeholder : </span><span id="stakeholder_on_card1">Teachers</span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- <div id="MyEvents">
     <div class="container" style="padding: 50px 0;">
         <!-- Events -->
       @foreach ($u_events as $uv)
@@ -160,7 +259,7 @@
          @endforeach 
       @endforeach    
     </div>
-</div>
+</div> --}}
 
 <!-- JQuery -->
 
@@ -181,6 +280,19 @@
     $(document).ready(function() {
         $('.modal').modal();
     });
+
+    $('.card_to_update_modal').click(function() {
+            var id_no = this.id;
+            document.getElementById("day_on_modal").innerHTML = document.getElementById("day_on_card" + id_no).innerHTML;
+            document.getElementById("date_on_modal").innerHTML = document.getElementById("date_on_card" + id_no).innerHTML;
+            document.getElementById("time_on_modal").innerHTML = document.getElementById("time_on_card" + id_no).innerHTML;
+            document.getElementById("title_on_modal").innerHTML = document.getElementById("title_on_card" + id_no).innerHTML;
+            document.getElementById("description_on_modal").innerHTML = document.getElementById("description_on_card" + id_no).innerHTML;
+            document.getElementById("venue_on_modal").innerHTML = document.getElementById("venue_on_card" + id_no).innerHTML;
+            document.getElementById("speaker_on_modal").innerHTML = document.getElementById("speaker_on_card" + id_no).innerHTML;
+            document.getElementById("organiser_on_modal").innerHTML = document.getElementById("organiser_on_card" + id_no).innerHTML;
+            document.getElementById("stakeholder_on_modal").innerHTML = document.getElementById("stakeholder_on_card" + id_no).innerHTML;
+        });
 
     </script>
 
